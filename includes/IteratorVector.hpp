84e4e4819eb6633dc4bce896faf8e24ddd604c40
andersonhsporto <anderson.higo2@gmail.com>
2022-10-28 02:15:04 -0300

    add: Vector Iterators

--- includes/IteratorVector.hpp
+++ includes/IteratorVector.hpp
@@ -5,4 +5,30 @@
 #ifndef FT_CONTAINERS_ITERATORVECTOR_HPP
 #define FT_CONTAINERS_ITERATORVECTOR_HPP
 
+#include "ft_iterator_base.hpp"
+
+namespace ft {
+
+// Random-access iterators are iterators that can be used to access elements at an arbitrary
+// offset position relative to the element they point to, offering the same functionality as pointers.
+template<typename T>
+class random_access_iterator {
+  // ****************** Member types ******************
+ public:
+  typedef T value_type;
+
+  typedef T *pointer;
+
+  typedef T &reference;
+
+  typedef std::ptrdiff_t difference_type;
+
+  typedef std::random_access_iterator_tag iterator_category;
+
+  // ****************** Member functions ******************
+ public:
+
+};
+};
+
 #endif //FT_CONTAINERS_ITERATORVECTOR_HPP
