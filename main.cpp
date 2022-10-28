d3f2f59adf1b77dcdd531543bfa4b3e9ba5b7a10
andersonhsporto <anderson.higo2@gmail.com>
2022-10-28 03:18:52 -0300

    add: vector tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
-#include "./includes/Vector.hpp" /// TODO: simplify this path
+#include "./includes/Vector.hpp"
+#include "./includes/Colors.hpp"
+#include <vector>
 
-int main () {
-  std::cout << "Hello World!" << std::endl;
+int main() {
+  std::vector<int> values;
+
+  values.push_back(1);
+  values.push_back(2);
+  values.push_back(3);
+  values.push_back(4);
+  values.push_back(5);
+
+  std::cout << "Not using iterators" << std::endl;
+  for (int i = 0; i < values.size(); i++) {
+    std::cout << CYAN << values[i] << RESET << std::endl;
+  }
+
+    std::cout << "Using iterators" << std::endl;
+  for (std::vector<int>::iterator it = values.begin(); it != values.end(); it++) {
+    std::cout << GREEN << *it << RESET << std::endl;
+  }
+
+  return 0;
 }
