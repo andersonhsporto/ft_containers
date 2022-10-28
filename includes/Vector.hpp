e292350043aaa4753f71bae67fef16ab6c3546f1
andersonhsporto <anderson.higo2@gmail.com>
2022-10-28 03:18:20 -0300

    refactor: using random access iterator

--- includes/Vector.hpp
+++ includes/Vector.hpp
@@ -5,11 +5,13 @@
 #ifndef FT_CONTAINERS_VECTOR_HPP
 #define FT_CONTAINERS_VECTOR_HPP
 
-#include <iostream>
+#include "IteratorVector.hpp"
 
 namespace ft {
 template<class T, class Allocator = std::allocator<T> >
 class vector {
+// ****************** Member types ******************
+
  public:
   typedef typename Allocator::reference reference;
 
@@ -27,22 +29,36 @@ class vector {
 
   typedef typename Allocator::const_pointer const_pointer;
 
+  typedef ft::random_access_iterator<value_type> iterator;
+
+  typedef ft::random_access_iterator<const value_type> const_iterator;
+
   // TODO: Implement iterator
+  //  typedef std::reverse_iterator<iterator> reverse_iterator;
+  //  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
 
-  //  typedef implementation defined iterator;
-  //  typedef implementation defined const_iterator;
-//  typedef std::reverse_iterator<iterator> reverse_iterator;
-//  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-
-// ***********************************************************************************************//
-// ***********************************************************************************************//
-// ***********************************************************************************************//
-// ***********************************************************************************************//
+// ****************** Member functions ******************
  public:
 
-  // construct / copy / destroy:
+  // default constructor without parameters
   vector() : _size(0), _capacity(0), _allocator(Allocator()) {
     _data = NULL;
+  }
+
+  // default constructor with allocator parameter
+  explicit vector(const allocator_type &alloc) : _size(0), _capacity(0), _allocator(alloc) {
+    _data = NULL;
+  }
+
+  // fill constructor
+  explicit vector(size_type n, const value_type &val = value_type(),
+                  const allocator_type &alloc = allocator_type())
+      : _size(n), _capacity(n), _allocator(alloc) {
+
+    _data = _allocator.allocate(_capacity);
+    for (size_type i = 0; i < _size; i++) {
+      _allocator.construct(_data + i, val);
+    }
   }
 
 
