67354a17fc7541629cd49eb1d7cb2e692ab1bdbb
andersonhsporto <anderson.higo2@gmail.com>
2022-11-20 00:40:17 -0300

    refactor: new struct in AVL member struct

--- includes/Tree/AVL.hpp
+++ includes/Tree/AVL.hpp
@@ -8,8 +8,18 @@
 #include <cstddef>
 #include "Node.hpp"
 
+enum class BalanceFactor {
+  LEFT_HEAVY = -1,
+  BALANCED = 0,
+  RIGHT_HEAVY = 1
+};
+
 namespace ft {
-template<typename T, class compare, class allocator>
+template<
+    typename T,
+    class compare,
+    class allocator
+>
 class AVL {
   // *********************************************************************************************
   // *********************************************************************************************
@@ -43,135 +53,24 @@ class AVL {
     return *this;
   }
 
-  ~AVL() {
-    clear();
-  }
+  ~AVL() {}
 
-  // ********************************************************************************************
-  // ********************************************************************************************
-  // *************************************** Member Functions ***********************************
-
-// TODO:: ?? PRIVATE
-  node_type *newNode(const value_type &data) {
-    node_type *node = _alloc.allocate(1);
-
-    _alloc.construct(node, data);
-    return node;
-  }
-
-  node_type *insert(node_type *n1, const value_type &data) {
-    if (!n1) {
-      return newNode(data);
-    } else if (data < n1->data) {
-      n1->left = insert(n1->left, data);
-      if (height(n1->left) - height(n1->right) == 2) {
-        if (data < n1->left->data) {
-          n1 = rotateWithLeftChild(n1);
-        } else {
-          n1 = doubleWithLeftChild(n1);
-        }
+  // Assigns all nodes
+  void assign(const node_type &node) {
+    if (node) {
+      assign(node->left);
+      if (node.data) {
+        insert(node.data);
       }
-    } else if (data > n1->data) {
-      n1->right = insert(n1->right, data);
-      if (height(n1->right) - height(n1->left) == 2) {
-        if (data > n1->right->data) {
-          n1 = rotateWithRightChild(n1);
-        } else {
-          n1 = doubleWithRightChild(n1);
-        }
-      }
-    }
-    updateNodeHeight(n1);
-    return n1;
-  }
-
-  // TODO:: ?? PRIVATE
-  node_type *singleRightRotate(node_type *k2) {
-    node_type *k1 = k2->left;
-
-    k2->left = k1->right;
-    k1->right = k2;
-    updateNodeHeight(k2);
-    updateNodeHeight(k1);
-    return k1;
-  }
-
-  // TODO:: ?? PRIVATE
-  node_type *singleLeftRotate(node_type *k2) {
-    node_type *k1 = k2->right;
-
-    k2->right = k1->left;
-    k1->left = k2;
-    updateNodeHeight(k2);
-    updateNodeHeight(k1);
-    return k1;
-  }
-
-  // TODO: ?? PRIVATE
-  void updateNodeHeight(node_type *node) {
-    node->height = max(height(node->left), height(node->right)) + 1;
-  }
-
-  // TODO: ?? PRIVATE
-  node_type *doubleLeftRotate(node_type *k3) {
-    k3->right = singleRightRotate(k3->right);
-    return singleLeftRotate(k3);
-  }
-
-  // TODO: ?? PRIVATE
-  node_type *doubleRightRotate(node_type *k3) {
-    k3->left = singleLeftRotate(k3->left);
-    return singleRightRotate(k3);
-  }
-
-  // Recursive member function to return the minimum node in the tree
-  // This function simply return the leftmost node in the tree
-  node_type *findMin(node_type *node) {
-    if (node == NULL)
-      return NULL;
-    else if (node->left == NULL)
-      return node;
-    else
-      return findMin(node->left);
-  }
-
-  // Recursive member function to return the maximum node in the tree
-  // This function simply returns the rightmost node in the tree
-  node_type *findMax(node_type *node) {
-    if (node == NULL)
-      return NULL;
-    else if (node->right == NULL)
-      return node;
-    else
-      return findMax(node->right);
-  }
-
-  // Recursive deallocate all nodes in the tree
-  void clear(node_type *node) {
-    if (node != NULL) {
-      clear(node->left);
-      clear(node->right);
-      _alloc.deallocate(node, 1);
+      assign(node->right);
     }
   }
 
-  // Return the height of node
-  int height(node_type *node) {
-    return (node == NULL ? -1 : node->height);
+  // Insert a node
+  node_type *insert(value_type value) {
+    this->_root = insertNode(this->_root, value);
+    return this->_root;
   }
-
-  // Return the balance factor of node
-  int getBalance(node_type *node) {
-    if (node == NULL)
-      return 0;
-    else
-      return height(node->left) - height(node->right);
-  }
-
-
-
-
-
 
 
 // ********************************************************************************************
@@ -188,6 +87,111 @@ class AVL {
 // ********************************************************************************************
 // ********************************************************************************************
 // *************************************** Member Functions ***********************************
+
+  // Get the height of a given node
+  int getNodeHeight(node_pointer node) {
+    if (node) {
+      return node->height;
+    }
+    return 0;
+  }
+
+  // Update the height of a given node
+  void updateNodeHeight(node_pointer node) {
+    node->height = 1 + max(getNodeHeight(node->left), getNodeHeight(node->right));
+  }
+
+  // Right rotation of a given node
+  node_type *rotateRight(node_type *node) {
+    node_type *left = node->left;
+    node_type *leftRight = left->right;
+
+    left->right = node;
+    node->left = leftRight;
+
+    updateNodeHeight(node);
+    updateNodeHeight(left);
+
+    return left;
+  }
+
+  // Left rotation of a given node
+  node_type *rotateLeft(node_type *node) {
+    node_type *right = node->right;
+    node_type *rightLeft = right->left;
+
+    right->left = node;
+    node->right = rightLeft;
+
+    updateNodeHeight(node);
+    updateNodeHeight(right);
+
+    return right;
+  }
+
+  // Create a new node with a given value
+  node_type *fromValue(const value_type &value) {
+    node_type *node = _alloc.allocate(1);
+
+    _alloc.construct(node, value);
+    return node;
+  }
+
+  // Get the balance factor of a given node
+  BalanceFactor unbalancedTree(const node_pointer node) {
+    if (node) {
+      int leftHeight = getNodeHeight(node->left);
+      int rightHeight = getNodeHeight(node->right);
+
+      if (leftHeight - rightHeight > 1) {
+        return BalanceFactor::LEFT_HEAVY;
+      } else if (rightHeight - leftHeight > 1) {
+        return BalanceFactor::RIGHT_HEAVY;
+      }
+    }
+  }
+
+  // Balance a branch of a given node
+  node_type *balance(node_type *node, const value_type &value) {
+    if (unbalancedTree(node) == BalanceFactor::LEFT_HEAVY) {
+      if (_comp(value, node->left->value)) {
+        node = rotateRight(node);
+      } else {
+        node->left = rotateLeft(node->left);
+        node = rotateRight(node);
+      }
+    } else if (unbalancedTree(node) == BalanceFactor::RIGHT_HEAVY) {
+      if (_comp(node->right->value, value)) {
+        node = rotateLeft(node);
+      } else {
+        node->right = rotateRight(node->right);
+        node = rotateLeft(node);
+      }
+    }
+  }
+
+  // Insert a new node in the tree and balance it
+  node_type *insertNode(node_type *node, const value_type &value) {
+    // If the tree is empty, create a new node
+    if (!node) {
+      return fromValue(value);
+    }
+
+    // Comp is a function object that returns true if the first argument is less than the second
+    if (_comp(value, node->value)) {
+      node->left = insertNode(node->left, value);
+    } else if (_comp(node->value, value)) {
+      node->right = insertNode(node->right, value);
+    } else {
+      return node;
+    }
+
+    // Update the height of the node
+    updateNodeHeight(node);
+
+    // Balance the tree if it is unbalanced
+    return balance(node, value);
+  }
 
 };
 }
